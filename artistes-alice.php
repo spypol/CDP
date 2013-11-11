6f08d18af956a2101b947649a4cd6ac8fe2c8c16
spypol <paulpinier@gmail.com>
2013-11-11 09:23:01 -0500

    Page artiste Alice

--- artistes-alice.php
+++ artistes-alice.php
@@ -1,49 +1,75 @@
 <?php include('include/head-alice.php'); ?>
 
-		<?php include('include/slider-alice.php'); ?>
-		
-        <div class="main-container">
-            <div class="main wrapper clearfix">
-				
-				<?php include('include/menu.php'); ?>
+	<?php include('include/slider-alice.php'); ?>
+	
+	<div class="main-container">
+		<div class="main wrapper clearfix">
 			
-				<!-------------------------- ************************** -------------------------->
-				<!-------------------------- MAIN ARTISTES ALICE COLUMN -------------------------->
-				<!-------------------------- ************************** -------------------------->
-                <div id="articles">
-					
-					<!-------------------------- *************** -------------------------->
-					<!-------------------------- ARTISTES COLUMN -------------------------->
-					<!-------------------------- *************** -------------------------->
-					<article id="troupe">
-						<section>
-							<h1>La Troupe</h1>
-							<ul class="js-masonry" data-masonry-options='{ "columnWidth": 255, "itemSelector": "li", "gutter": 25 }'>
-								<li><span class="circular" id="antonio"></span><h3>Antonio Macipe</h3><h4>Antonio est Peter Pan</h4></h3>
-									<p>Apr&egrave;s 12 ans de formation artistique et d'exp&eacute;riences sur sc&egrave;ne au Venezuela, son pays d'origine, Antonio arrive en France en 2009. Sa passion pour le chant lyrique et populaire, mais &eacute;galement la danse et le th&eacute;&acirc;tre l'incite &agrave; reprendre sa vie artistique et &agrave; remonter sur sc&egrave;ne. Il interpr&egrave;te Peter Pan ce qui constitue pour lui un v&eacute;ritable d&eacute;fi: son premier grand r&ocirc;le en langue fran&ccedil;aise ! </p></li>
-								<li><span class="circular" id="joe"></span><h3>Joe Marshall</h3><h4>Joe est Wendy</h4>
-									<p>Joe a commenc&eacute; le chant &agrave; l&rsquo;&acirc;ge de 8 ans &agrave; L&rsquo;Acad&eacute;mie du Chant Moderne. Form&eacute;e et coach&eacute;e en studio par Chris Vincent, elle parfait sa formation lors de plusieurs Master Classes avec Richard Cross. Apr&egrave;s avoir chant&eacute; sur sc&egrave;ne notamment aupr&egrave;s de Julie Zenatti et Dave, Joe pr&eacute;pare actuellement la sortie de son 3e single. L&rsquo;exp&eacute;rience &laquo; Souviens-toi Pan &raquo; est pour elle une occasion de plus de progresser artistiquement et de partager sa passion au sein d&rsquo;une troupe enthousiaste. Elle interpr&egrave;te le r&ocirc;le de Wendy. </p></li>
-								<li><span class="circular" id="julie"></span><h3>Julie Lemas</h3><h4>Julie est Mouche</h4>
-									<p>Chant, Th&eacute;atre, Danse, tous sont naturels pour Julie qui, avec sa sensibilit&eacute; et sa voix, a d&eacute;j&agrave; une exp&eacute;rience sc&eacute;nique et artistique vari&eacute;e. Son portfolio inclut &laquo; Le No&euml;l de Monsieur Scroodge &raquo;, spectacle musical bas&eacute; sur le conte de Charles Dickens et &laquo; La Galette des Rois &raquo;, com&eacute;die d&rsquo;apr&egrave;s Roger-Ferdinand avec la compagnie Le Grenier de Bruy&egrave;res. C&rsquo;est sa rencontre avec Philippe Padovani, son professeur depuis 10 ans, qui lui ouvre les portes du chant. Suivront &laquo; Show Show Broadway &raquo; et &laquo; La Beloboidormant &raquo;, spectacle pour enfants adapt&eacute;e du conte de Charles Perrault par la compagnie des Lutins Oranges. Apr&egrave;s une ann&eacute;e au sein de l'AICOM o&ugrave; Julie perfectionne son talent, c'est en 2008 qu'elle monte sur les planches d'une sc&egrave;ne parisienne avec la Com&eacute;die Musicale &quot;Je m'voyais d&eacute;j&agrave;&quot;. Spectacle de Laurent Ruquier autour des chansons de Charles Aznavour, jou&eacute; au Th&eacute;&acirc;tre du Gymnase, au Th&eacute;&acirc;tre Comedia et en Tourn&eacute;e en France.</p></li>
-								<li><span class="circular" id="ludo"></span><h3>Ludovic Fert</h3><h4>Ludovic est Jean</h4>
-									<p>Ludovic, qui interpr&egrave;te le r&ocirc;le du petit fr&egrave;re dans &laquo; Souviens-toi Pan &raquo;, est un autodidacte qui poursuit des &eacute;tudes scientifiques lorsqu&rsquo;il est s&eacute;lectionn&eacute; dans la troupe de Souviens-toi-Pan en 2005. L&rsquo;ann&eacute;e suivante, il renouvelle l&rsquo;aventure en s&rsquo;impliquant dans une autre com&eacute;die musicale et participe &agrave; plusieurs spectacles de danse en parall&egrave;le. Quelques ann&eacute;es plus tard, la sc&egrave;ne lui manque et il saisit l&rsquo;opportunit&eacute; de la r&eacute;&eacute;criture du spectacle &laquo; Souviens-toi Pan &raquo; pour se lancer dans l&rsquo;aventure.</p></li>
-								<li><span class="circular" id="maeva"></span><h3>Ma&euml;va Clamaron</h3><h4>Ma&euml;va est Clochette</h4>
-									<p>A l'&acirc;ge de sept ans, Ma&euml;va a int&eacute;gr&eacute; la Ma&icirc;trise de l'Op&eacute;ra de Lyon. Depuis, les repr&eacute;sentations se sont encha&icirc;n&eacute;es et la musique ne l&rsquo;a jamais quitt&eacute;e. Son premier album est en cours de pr&eacute;paration. Elle a d&eacute;cid&eacute; d&rsquo;int&eacute;grer l&rsquo;&eacute;quipe Souviens-toi Pan afin de s&rsquo;investir dans un beau projet et vivre &agrave; nouveau une exp&eacute;rience sur sc&egrave;ne. Elle joue le r&ocirc;le de Clochette.</p></li>
-								<li><span class="circular" id="maud"></span><h3>Maud Abeloos</h3><h4>Maud est Lily</h4>
-									<p>Maud a pris go&ucirc;t au th&eacute;&acirc;tre tr&egrave;s jeune &agrave; travers des ouvrages de Pierre Gripari et Yak Rivais. Durant ses &eacute;tudes de droit, elle a continu&eacute; &agrave; exercer sa passion pour le th&eacute;&acirc;tre et le chant  en allant voir r&eacute;guli&egrave;rement des pi&egrave;ces et en &eacute;tant chanteuse dans un groupe. Dans &laquo; Souviens-toi Pan &raquo;, le dynamisme de Maud s&rsquo;exprime &agrave; travers Lily, un personnage espi&egrave;gle et taquin.</p></li>
-								<li><span class="circular" id="melanie"></span><h3>M&eacute;lanie Duchesne</h3><h4>M&eacute;lanie est Poussi&egrave;re</h4>
-									<p>M&eacute;lanie a commenc&eacute; &agrave; prendre des cours de chant &agrave; 11 ans. A 13 ans, cette jeune chanteuse belge est laur&eacute;ate de l&rsquo;&eacute;mission &laquo; Pour la Gloire &raquo; sur la RTBF, dans la cat&eacute;gorie junior. Elle a ensuite donn&eacute; des concerts et assur&eacute; entre autres les premi&egrave;res parties d&rsquo;Annie Cordy, Julien Clerc, Patrick Juvet et Daniel Guichard. Actuellement &agrave; Paris pour la pr&eacute;paration de son premier album, elle interpr&egrave;te sur sc&egrave;ne le r&ocirc;le d&rsquo;une enfant perdue, Poussi&egrave;re.</p></li>
-								<li><span class="circular" id="nico"></span><h3>Nicolas Tatossian</h3><h4>Nicolas est le narrateur</h4>
-									<p>Nicolas d&eacute;veloppe sa passion pour la vari&eacute;t&eacute; fran&ccedil;aise lors de sa formation aupr&egrave;s d&rsquo;Armande Alta&iuml;, puis au sein de l&rsquo;&eacute;cole de Richard Cross. Il s&rsquo;est d&eacute;j&agrave; produit sur sc&egrave;ne lors de divers concours et concerts donn&eacute;s notamment en Arm&eacute;nie. La com&eacute;die musicale &laquo; Souviens-toi Pan &raquo; est l&rsquo;occasion pour lui de s&rsquo;&eacute;panouir	sur sc&egrave;ne et de progresser. Il interpr&egrave;te le r&ocirc;le du narrateur.</p></li>
-								<li><span class="circular" id="ralph"></span><h3>Ralph Folio</h3><h4>Ralph est le Capitaine Crochet</h4>
-									<p>C&rsquo;est lors de sa formation comme com&eacute;dien au Cours Florent qu&rsquo;il se passionne pour la com&eacute;die musicale. Il se forme alors intensivement en danse et en chant, puis devient, au terme de son cursus, assistant de la section &laquo; Dancing &raquo; au Cours Florent. Avec &laquo; Hair &raquo;, il a obtenu  son premier r&ocirc;le important en com&eacute;die musicale, il jouera ensuite dans le &quot;Rocky Horror Pictures show&quot; et &quot;Evita&quot; puis il jonglera avec le th&eacute;&acirc;tre dans des pi&egrave;ces tel que &quot;Embrassons nous Folleville&quot;, &quot;Demain on se marie&quot; mais aussi avec des spectacles musicaux &quot;PBA&quot;, &quot;Changer de vie&quot;, &quot;La chanson c'est pas d'la chansonnette&quot;... et enfin &quot;Souviens Toi Pan&quot;, o&ugrave; il interpr&egrave;te le personnage du Capitaine Crochet. </p></li>
-								<li><span class="circular" id="raphaelle"></span><h3>Rapha&euml;lle Raimon</h3><h4>Rapha&euml;lle est la chef Indienne</h4>
-									<p>Rapha&euml;lle a commenc&eacute; la musique en jouant de la clarinette, pour se tourner plus tard vers le chant lyrique. Son m&eacute;tier de professeur de musique au coll&egrave;ge lui a permis de bousculer sa formation classique, pour se rapprocher de l&rsquo;univers musical de Peter Pan. Elle interpr&egrave;te le r&ocirc;le de la Chef Indienne dans la com&eacute;die musicale, une nouvelle exp&eacute;rience qui lui permet d&rsquo;allier danse et chant.</p></li>
-							</ul>
-						</section>
-					</article>
+			<?php include('include/menu.php'); ?>
+		
+			<!-------------------------- ************************** -------------------------->
+			<!-------------------------- MAIN ARTISTES ALICE COLUMN -------------------------->
+			<!-------------------------- ************************** -------------------------->
+			<div id="articles">
+				<div id="photo-vignettes">					
+					<!-------------------------- COMEDIES COLUMN -------------------------->
+					<div id="photo-comedies">
+						<h1>Les Com&eacute;dies</h1>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+					</div>
+				
+					<!-------------------------- CREATION COLUMN -------------------------->
+					<div id="photo-creation">
+						<h1>La Cr&eacute;ation</h1>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+					</div>
+				
+					<!-------------------------- TECHNIQUE COLUMN -------------------------->
+					<div id="photo-technique">
+						<h1>La Technique</h1>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+					</div>
 				</div>
-            </div> <!-- #main -->
-        </div> <!-- #main-container -->
+				
+				<div id="details">
+					<!-------------------------- Photo -------------------------->
+					<div id="artiste-portrait">
+						<img src="_photos/prod2013/paulpinier.jpg" class="portrait"/>
+					</div>
+					
+					<!-------------------------- Photo -------------------------->
+					<div id="artiste-details">
+						Nom Prenom<br />
+						Role<br />
+						Situation<br />
+						Facebook<br />
+						Ses anciens spectacles<br />
+					</div>
+					
+					<!-------------------------- Photo -------------------------->
+					<div id="artiste-gallerie">
+						<div class="slider-wrapper theme-default">
+							<div id="photos-stp" class="nivoSlider">
+								<a href="_photos/SPECTACLES/STP/DSC_0129.jpg" data-lightbox-gallery="gallery-stp" class="preview-image"><img src="_photos/SPECTACLES/STP/DSC_0129.jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0129.jpg" alt=""/></a>
+								<a href="_photos/SPECTACLES/STP/DSC_0031.jpg" data-lightbox-gallery="gallery-stp" class="preview-image"><img src="_photos/SPECTACLES/STP/DSC_0031.jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0031.jpg" alt=""/></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> <!-- #main -->
+	</div> <!-- #main-container -->
 		
 <?php include('include/footer.php'); ?>
