2ceac07e1ff1fb6f199b9c852f1a0c7ff0a3850a
spypol <paulpinier@gmail.com>
2013-11-11 19:53:39 -0500

    Artistes Alice Page

--- artistes-alice.php
+++ artistes-alice.php
@@ -15,12 +15,11 @@
 					<!-------------------------- COMEDIES COLUMN -------------------------->
 					<div id="photo-comedies">
 						<h1>Les Com&eacute;dies</h1>
-						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
-						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
-						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
-						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
-						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
-						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
+						<img src="_photos/troupe/alice/avatars/antonio.jpg" class="vignette"/>
+                        <img src="_photos/troupe/alice/avatars/julie.jpg" class="vignette"/>
+                        <img src="_photos/troupe/alice/avatars/marie.jpg" class="vignette"/>
+                        <img src="_photos/troupe/alice/avatars/veronique.jpg" class="vignette"/>
+                        <img src="_photos/troupe/alice/avatars/vincent.jpg" class="vignette"/>
 					</div>
 				
 					<!-------------------------- CREATION COLUMN -------------------------->
@@ -33,37 +32,47 @@
 					</div>
 				
 					<!-------------------------- TECHNIQUE COLUMN -------------------------->
-					<div id="photo-technique">
+					<!--<div id="photo-technique">
 						<h1>La Technique</h1>
 						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
 						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
 						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
 						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
 						<img src="_photos/prod2013/paulpinier.jpg" class="vignette"/>
-					</div>
+					</div>-->
 				</div>
 				
 				<div id="details">
 					<!-------------------------- Photo -------------------------->
 					<div id="artiste-portrait">
-						<img src="_photos/prod2013/paulpinier.jpg" class="portrait"/>
+						<img src="_photos/troupe/alice/avatars/antonio.jpg" class="portrait"/>
 					</div>
 					
 					<!-------------------------- Photo -------------------------->
 					<div id="artiste-details">
-						Nom Prenom<br />
+						Antonio Macipe<br />
 						Role<br />
-						Situation<br />
+						Single<br />
 						Facebook<br />
-						Ses anciens spectacles<br />
+						Souviens-toi Pan !<br />
+                        
+                        <p>Cours de chant et musique depuis ses 5 ans, instructeur en danse latine (salsa) depuis ses 17 ans, formation d'acteur depuis ses 18 ans, Antonio arrive en France en 2009 pour développer ses compétences artistiques et professionnelles (journaliste de formation académique) sans parler la langue. Ayant une bonne expérience en comédies musicales et concerts au Venezuela (12 ans d'expériences sur scène), Antonio décroche son premier rôle principal en français, à Paris, en 2011; et sa première nomination en tant que meilleur comédien dans un second rôle en 2013 (Petits Molières). Il continue à exploiter ses envies de scène à travers différents projets professionnels et académiques afin d'exercer le constant enrichissement de l'artiste.</p>
 					</div>
 					
 					<!-------------------------- Photo -------------------------->
 					<div id="artiste-gallerie">
 						<div class="slider-wrapper theme-default">
 							<div id="photos-stp" class="nivoSlider">
-								<a href="_photos/SPECTACLES/STP/DSC_0129.jpg" data-lightbox-gallery="gallery-stp" class="preview-image"><img src="_photos/SPECTACLES/STP/DSC_0129.jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0129.jpg" alt=""/></a>
-								<a href="_photos/SPECTACLES/STP/DSC_0031.jpg" data-lightbox-gallery="gallery-stp" class="preview-image"><img src="_photos/SPECTACLES/STP/DSC_0031.jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0031.jpg" alt=""/></a>
+								<a href="_photos/troupe/alice/Antonio Macipe.jpg" data-lightbox-gallery="gallery-stp" class="preview-image">
+                                    <img src="_photos/troupe/alice/Antonio Macipe.jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0129.jpg" alt=""/></a>
+								<a href="_photos/troupe/alice/Antonio Macipe (2).jpg" data-lightbox-gallery="gallery-stp" class="preview-image">
+                                    <img src="_photos/troupe/alice/Antonio Macipe (2).jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0031.jpg" alt=""/></a>
+                                <a href="_photos/troupe/alice/Antonio Macipe (3).jpg" data-lightbox-gallery="gallery-stp" class="preview-image">
+                                    <img src="_photos/troupe/alice/Antonio Macipe (3).jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0031.jpg" alt=""/></a>
+                                <a href="_photos/troupe/alice/Antonio Macipe (4).jpg" data-lightbox-gallery="gallery-stp" class="preview-image">
+                                    <img src="_photos/troupe/alice/Antonio Macipe (4).jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0031.jpg" alt=""/></a>
+                                <a href="_photos/troupe/alice/Antonio Macipe (5).jpg" data-lightbox-gallery="gallery-stp" class="preview-image">
+                                    <img src="_photos/troupe/alice/Antonio Macipe (5).jpg" data-thumb="_photos/SPECTACLES/STP/DSC_0031.jpg" alt=""/></a>
 							</div>
 						</div>
 					</div>
