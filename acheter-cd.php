d267d04c9956fb90b1e6cb3ef7d7f8e3c7f5ef58
spypol <paulpinier@gmail.com>
2013-12-05 14:21:06 -0500

    Page acheter

--- acheter-cd.php
+++ acheter-cd.php
@@ -1,13 +1,13 @@
 <?php
-	$email = '';
+	//$email = '';
 
-	if (isset($_POST['email'])) { $email = $_POST['email']; }
+	//if (isset($_POST['email'])) { $email = $_POST['email']; }
     
-    $from = 'From: compotedeprod.com'; 
-    $to = 'contact@compotedeprod.com'; 
-    $subject = 'Réservation CD Alice';
+    //$from = 'From: compotedeprod.com'; 
+    //$to = 'contact@compotedeprod.com'; 
+    //$subject = 'Réservation CD Alice';
 
-    $body = "$email\n vient de réserver un CD Alice";
+    //$body = "$email\n vient de réserver un CD Alice";
 ?>
 
 <?php include('include/head.php'); ?>
@@ -30,36 +30,47 @@
 					<!-------------------------- *************** -------------------------->
 					<article id="acheter-cd-alice">
 						<section>
-							<h1>Bientôt le CD 2 titres de Alice !</h1>
+							<h1>Le 2 titres d'Alice en vente ici!</h1>
                             
                             <p>Comme Alice, laissez-vous guider par le Chat du Cheshire et la Chenille, jazzy et sensuelle. Saurez-vous reconnaitre et attraper le Lapin Blanc ? Pour le savoir achetez le CD 2 titres de la comédie musicale "Alice" et découvrez les toutes premières chansons interprétées par la troupe.</p>
                             <img src="img/cd-alice.png" title="CD Alice" id="cd-alice" />
                             
-                            <?php
-								if (isset($_POST['submit'])) {
-									if ($email != '') {			 
-                                         if (mail ($to, $subject, $body, $from)) { 
-                                             echo "<span class='message_vert'>Votre r&eacute;servation a bien &eacute;t&eacute; envoy&eacute;e !</span>";
-                                         } else { 
-                                             echo "<span class='message_rouge'>Oops, nous avons eu un probl&egrave;me, essayez d'envoyer directement un message &agrave; contact@compotedeprod.com !</span>"; 
-                                         }
-									} else {
-										echo "<span class='message_rouge'>Vous devez remplir tous les champs, merci !</span>";
-									}
-								} else {?>
                             
-                                    <p><i>Envoyez-nous votre email pour rester au courant de la sortie du CD</i></p>
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paypal">
+                                <input type="hidden" name="cmd" value="_s-xclick">
+                                <input type="hidden" name="hosted_button_id" value="9EBBMUGWFUKYG">
+                                <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
+                                <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1" class="paypalsmall">
+                            </form>
+
+                            
+                            
+                            
+                            <?php
+								//if (isset($_POST['submit'])) {
+								//	if ($email != '') {			 
+                                //         if (mail ($to, $subject, $body, $from)) { 
+                                //             echo "<span class='message_vert'>Votre r&eacute;servation a bien &eacute;t&eacute; envoy&eacute;e !</span>";
+                                //         } else { 
+                                //             echo "<span class='message_rouge'>Oops, nous avons eu un probl&egrave;me, essayez d'envoyer directement un message &agrave; contact@compotedeprod.com !</span>"; 
+                                //         }
+								//	} else {
+								//		echo "<span class='message_rouge'>Vous devez remplir tous les champs, merci !</span>";
+								//	}
+								//} else {?>
+                            
+                                <!--    <p><i>Envoyez-nous votre email pour rester au courant de la sortie du CD</i></p>
                             
                                     <form method="post" action="acheter-cd.php">
                                                 
-                                        <input name="email" type="email" placeholder="Email" value="<?php echo $email; ?>">
+                                        <input name="email" type="email" placeholder="Email" value="<?php //echo $email; ?>">
                                                 
                                         <input id="submit" class="bouton-rouge" name="submit" type="submit" value="ENVOYER">
                                         
                                     </form>
+                                -->
                             
-                                <?php }
-							?>
+                                <?php //}?>
 							
 						</section>
 					</article>
@@ -74,7 +85,7 @@
                             <p>Afin de découvrir l'univers de "Souviens-toi, Pan!", achetez le CD live de la comédie musicale !</p>
                             <img src="img/cd-stp.png" title="CD Souviens toi Pan" />
                             
-                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" id="paypal">
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="paypal">
                                 <input type="hidden" name="cmd" value="_s-xclick">
                                 <input type="hidden" name="hosted_button_id" value="DA7MM57C4M2EA">
                                 <table>
@@ -86,7 +97,7 @@
                                 </table>
                                 <input type="hidden" name="currency_code" value="EUR">
                                 <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
-                                <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1" id="paypalsmall">
+                                <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1" class="paypalsmall">
                             </form>
 
                           
