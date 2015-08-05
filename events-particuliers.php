313b56f41c1f0308a49e98e3c95957c621e6b24c
spypol <paulpinier@gmail.com>
2015-08-04 22:47:36 -0400

    Events

---
+++ events-particuliers.php
@@ -0,0 +1,65 @@
+<?php include('include/head.php'); ?>
+
+		<?php include('include/slider.php'); ?>
+
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+				
+				<?php include('include/menu.php'); ?>
+                
+                <!-------------------------- *********** -------------------------->
+				<!-------------------------- PROD COLUMN -------------------------->
+				<!-------------------------- *********** -------------------------->
+                <div id="articles">
+                    <article id="" class="events-4">
+                        <h1>Animations Classiques</h1>
+                        <ul>
+                            <li>DJ (classique et DJ Live)</li>
+                            <li>Magicien</li>
+                            <li>Photographe</li>
+                            <li>Maitre de cérémonie</li>
+                        </ul>
+                        <img src="events/animations-classiques.jpg" alt="animations classiques" />
+                    </article>
+                    
+                    <article id="" class="events-4">
+                        <img src="events/animations-mariages.jpg" alt="animations mariages" />
+                        <h1>Animations Mariages</h1>
+                        <ul>
+                            <li>Enterrement de vie de jeune fille</li>
+                            <li>Enterrement de vie de garçon</li>
+                            <li>Préparation au mariage (mini-stage de danse/de chant</li>
+                        </ul>
+                    </article>
+
+                    <article id="" class="events-4">					
+                        <h1>Animations Artistiques</h1>
+                        <ul>
+                            <li>Groupe à la carte</li>
+                            <li>Concert privé (rock, jazz…)</li>
+                            <li>Show comédie musicale</li>
+                            <li>Karaoké Quizz (musiciens en live)</li>
+                            <li>Caricaturiste</li>
+                        </ul>
+                        
+                        <img src="events/animations-artistiques.jpg" alt="animations artistiques" />
+                        
+                    </article>
+
+                    <article id="" class="events-4">
+                        <h1>Animations Jeune Public</h1>
+                        <ul>
+                            <li>Clown</li>
+                            <li>Maquillage artistique</li>
+                            <li>Montez votre spectacle !</li>
+                            <li>Conteur d’histoires</li>
+                            <li>Père Noël</li>
+                            <li>Grand Jeu</li>
+                        </ul>
+                    </article>
+                </div>
+				
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+		
+<?php include('include/footer.php'); ?>
